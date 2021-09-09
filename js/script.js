c7b275e689171fa69cbb09445add6e85e4493a81
Lucas Medina <lucasgmedina96@gmail.com>
2021-09-08 23:42:54 -0300

    Js implemented in "Send" and "Open" Buttons

--- js/script.js
+++ js/script.js
@@ -4,3 +4,17 @@ function myFunc()  {
     document.getElementById('idDate').innerHTML = "Date: " + time;
 }
 setInterval(myFunc);
+
+function ChangeClass(){
+
+    document.getElementById("sendMessage").className = "btn btn-primary active"
+
+    document.getElementById("openMessage").className = "btn btn-primary";
+}
+
+function ChangeClass2(){
+
+    document.getElementById("openMessage").className = "btn btn-primary active";
+
+    document.getElementById("sendMessage").className = "btn btn-primary";
+}
